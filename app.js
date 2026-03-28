0059c6bbd1301af5090bc7c9faece5c09d1a4792
Kevin1504 <kgomez@3ringenieria.com>
2026-03-27 23:48:38 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const sql = require("mssql");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+// CONFIGURACIÓN DE SQL SERVER
+const config = {
+  user: "sa3R",
+  password: "Mauricio2004",
+  server: "191.98.160.84",
+  port: 1433,
+  database: "GesNube",
+  options: {
+    encrypt: false,
+    trustServerCertificate: true
+  }
+};
+
+// ===============================
+//   RUTA 1: STOCK
+// ===============================
+app.get("/api/stock", async (req, res) => {
+  try {
+    const pool = await sql.connect(config);
+
+    const result = await pool.request().query(`
+      SELECT 
+        Codigo,
+        Articulo,
+        Almacen,
+        Stock,
+        CostoPromedioSoles,
+        CostoPromedioDolares,
+        EnTransito
+      FROM dbo.AlmCuboStock
+      WHERE EmpresaId = 22
+        AND Almacen = 'Principal'
+    `);
+
+    res.json(result.recordset);
+
+  } catch (err) {
+    console.error("ERROR API STOCK:", err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// ===============================
+//   RUTA 2: ARTÍCULOS
+// ===============================
+app.get("/api/articulos", async (req, res) => {
+  try {
+    const pool = await sql.connect(config);
+
+    const result = await pool.request().query(`
+      SELECT 
+        Codigo,
+        Articulo,
+        CodigoParte,
+        MarcaArticulo,
+        Familia,
+        Clase,
+        SubClase,
+        Laboratorio,
+        PesoNeto,
+        Origen,
+        Fabricante
+      FROM dbo.AlmCuboArticulos3R
+      WHERE EmpresaId = 22
+    `);
+
+    res.json(result.recordset);
+
+  } catch (err) {
+    console.error("ERROR API ARTICULOS:", err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// ===============================
+//   INICIAR SERVIDOR
+// ===============================
+const PORT = 4000; // <-- estás usando 4000 con ngrok
+app.listen(PORT, () => {
+  console.log("API escuchando en puerto " + PORT);
+});
